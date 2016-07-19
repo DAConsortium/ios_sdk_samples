d2c0b55b85bc4ffedd975461012c5791c95a4ebb
松崎健次郎 <kenjiro-matsuzaki@dac.co.jp>
2016-07-19 19:12:43 +0900

    release NativeAd for iOS 0.1.0

---
+++ DACNativeAdSDK/DACNativeAdSDK-Sample-ObjectiveC/DACNativeAdSDK-Sample-ObjectiveC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  DACNativeAdSDK-Sample-ObjectiveC
+//
+//  Copyright © 2016 D.A.Consortium Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
