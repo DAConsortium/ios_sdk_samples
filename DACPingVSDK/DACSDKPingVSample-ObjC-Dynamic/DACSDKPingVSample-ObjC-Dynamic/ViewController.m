cb23cef245bef756b5a863d629dead9e2918c1f7
松崎健次郎 <kenjiro-matsuzaki@dac.co.jp>
2016-10-20 11:22:30 +0900

    ping_sdk release

---
+++ DACPingVSDK/DACSDKPingVSample-ObjC-Dynamic/DACSDKPingVSample-ObjC-Dynamic/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DACSDKPingVSample-ObjC-Dynamic
+//
+//  Copyright (c) 2016 D.A.Consortium Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+@end
+
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    NSLog(@"----- viewDidLoad() begin -----");
+    [super viewDidLoad];
+    NSLog(@"----- viewDidLoad() end -----");
+}
+
+@end
